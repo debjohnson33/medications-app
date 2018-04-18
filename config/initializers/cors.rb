5532689ec12b5282608a76e3eaf229a0f4add6d2
debjohnson <debjohnson_11@yahoo.com>
2018-04-18 17:44:47 -0500

    Changed cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'localhost:3000', 'https://medications-app.heroku.com'
+     origins '*'
 
      resource '*',
        headers: :any,
